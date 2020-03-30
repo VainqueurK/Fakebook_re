a943dc35be8dad2f2ca2e3c7446fa0c1a4327b54
Tedis32 <61153139+Tedis32@users.noreply.github.com>
2020-03-30 19:37:54 +0100

    Tabs all included

---
+++ app/src/main/java/com/example/fakebookone/FragmentProfile.java
@@ -0,0 +1,28 @@
+package com.example.fakebookone;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentProfile extends Fragment {
+
+    View v;
+
+    public FragmentProfile()
+    {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_tab2, container, false);
+        return v;
+
+    }
+}
