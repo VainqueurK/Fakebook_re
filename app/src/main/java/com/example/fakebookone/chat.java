1857068836fa90b7e4b42b2952a8cf84bf24c38e
Tedis32 <61153139+Tedis32@users.noreply.github.com>
2020-03-30 20:34:48 +0100

    changed name to chat

--- app/src/main/java/com/example/fakebookone/chat.java
+++ app/src/main/java/com/example/fakebookone/chat.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link tab1#newInstance} factory method to
+ * Use the {@link chat#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class tab1 extends Fragment {
+public class chat extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,7 +24,7 @@ public class tab1 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public tab1() {
+    public chat() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class tab1 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment tab1.
+     * @return A new instance of fragment chat.
      */
     // TODO: Rename and change types and number of parameters
-    public static tab1 newInstance(String param1, String param2) {
-        tab1 fragment = new tab1();
+    public static chat newInstance(String param1, String param2) {
+        chat fragment = new chat();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
