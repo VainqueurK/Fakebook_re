bf7a9a3b455c7202211b2f91c1b4c0fc84da9a7b
Tedis32 <61153139+Tedis32@users.noreply.github.com>
2020-03-31 04:54:26 +0100

    Adding a Toolbar/Appbar to display throughout the app, changes made are in:  -"toolbar.xml" where I created the toolbar  -"activity_main.xml" where I implement the created toolbar  -"MainActivity.java" where I add the toolbar to onCreate() method  -"styles.xml" , here I changed the default action bar to "NoActionBar"
     at the moment I am getting errors with this, the project compiles and builds but the app itself won't run if I uncomment each line of code in the aforementioned files, if you'd like to take a look at it and see what the story is with it that'd be great but I'll be looking at it in the morning myself again

--- app/src/main/java/com/example/fakebookone/Activity_Chat.java
+++ app/src/main/java/com/example/fakebookone/Activity_Chat.java
@@ -1,12 +1,16 @@
 package com.example.fakebookone;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 
 /**
@@ -19,6 +23,15 @@ public class Activity_Chat extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private Toolbar chattoolbar;
+    private ImageButton sendMessageButton, sendImageFileButton;
+    private EditText sendUserMessage;
+    private RecyclerView userMessagesList;
+
+
+
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,7 +66,11 @@ public class Activity_Chat extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/fakebookone/MainActivity.java
+++ app/src/main/java/com/example/fakebookone/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fakebookone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
         TabLayout tabLayout = findViewById(R.id.tabBar);
         TabItem tab1 = findViewById(R.id.tab1);
