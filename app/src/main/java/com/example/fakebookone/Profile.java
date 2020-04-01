1c1a6f7e0ade83b343847ca21f99b00d65a13e81
James <jamesbrosnanballydwyer@gmail.com>
2020-04-01 14:22:19 +0100

    Set up firebase and included test sign up

--- app/src/main/java/com/example/fakebookone/Profile.java
+++ app/src/main/java/com/example/fakebookone/Profile.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link tab3#newInstance} factory method to
+ * Use the {@link Profile#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class tab3 extends Fragment {
+public class Profile extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,7 +24,7 @@ public class tab3 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public tab3() {
+    public Profile() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class tab3 extends Fragment {
      * @return A new instance of fragment tab3.
      */
     // TODO: Rename and change types and number of parameters
-    public static tab3 newInstance(String param1, String param2) {
-        tab3 fragment = new tab3();
+    public static Profile newInstance(String param1, String param2) {
+        Profile fragment = new Profile();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
